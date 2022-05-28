383ef76058a81c7864f0dca2c2507b8691eca820
KDA <iamkda@yandex.ru>
2022-05-28 03:05:20 +0300

    refactor: got rid of all selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -18,32 +18,34 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.classList.add("task-container");
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add("todo__checkbox");
     //label
     var label=document.createElement("label");//label
+    label.classList.add("todo__label");
     //input (text)
     var editInput=document.createElement("input");//text
+    editInput.classList.add("todo__task");
     //button.edit
     var editButton=document.createElement("button");//edit button
+    editButton.classList.add("todo__button", "todo__button-edit");
 
     //button.delete
     var deleteButton=document.createElement("button");//delete button
+    deleteButton.classList.add("todo__button", "todo__button-delete");
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
 
-    deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,11 +86,11 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".todo__button-edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .edit-mode
     if(containsClass){
-s
+
         //switch to .edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
@@ -156,8 +158,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".todo__button-edit");
+    var deleteButton=taskListItem.querySelector(".todo__button-delete");
 
 
     //Bind editTask to edit button.
